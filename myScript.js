84edde53252df4dcfce82816de08350ac21df27c
Efim Gutnik <bylochka96@gmail.com>
2021-09-17 22:04:25 +0300

    feet: add temporary modification of bg-color of selected block

--- myScript.js
+++ myScript.js
@@ -7,4 +7,27 @@ navigation.addEventListener('click', function (e) {
         behavior: "smooth",
         block: "start"
     })
+    document.getElementById(blockID).style.backgroundColor = '#f0bc81'
+    let timeOut = 0;
+    switch (blockID) {
+        case 'bio': {
+            timeOut = 1000;
+            break;
+        }
+        case 'skills':
+        case 'education':
+        case 'courses': {
+            timeOut = 1500
+            break;
+        }
+        case 'example':
+        case 'lang':
+        case 'contacts': {
+            timeOut = 2000
+            break;
+        }
+    }
+    setTimeout(() => {
+        document.getElementById(blockID).style.backgroundColor = '#fcfbf6'
+    }, timeOut)
 });
